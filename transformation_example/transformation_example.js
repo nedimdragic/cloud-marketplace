0633d431f2e12c8c6790249b8b2ea16d62a9815b
James Lingham <James@rhythmmax.co.uk>
2022-02-02 16:59:13 +0000

    Update transformation_example.js

--- transformation_example/transformation_example.js
+++ transformation_example/transformation_example.js
@@ -1,2 +1,5 @@
-console.log("This is a transformation example!")
-console.log([{{input}}].toString();
+transform(()=>{
+  console.log("This is a transformation example!")
+  console.log([{{input}}].toString();
+  console.log([{{multi_input}}].toString();
+})
