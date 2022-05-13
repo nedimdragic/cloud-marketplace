7623bc8824858def31763309973790a9f1d0b000
Nedim Dragic <50397376+nedimdragic@users.noreply.github.com>
2022-05-13 12:48:17 +0200

    Add files via upload

--- pii_hashing/pii_hashing.js
+++ pii_hashing/pii_hashing.js
@@ -3,7 +3,7 @@ import CryptoES from 'crypto-es';
 // "transform" function allows you to access event and apply changes
 transform((event) => {
     const overwrite = {{overwrite}};
-    const prefix = overwrite ? "" : {{prefix}};
+    const prefix = overwrite ? "" : "{{prefix}}";
     // demo purposes, before and after
     const before = {};
     const after = {};
@@ -13,19 +13,19 @@ transform((event) => {
 
         if (event?.data?.udo){
             pii_names.forEach((pii)=>{
-                if (typeof event.data.udo[pii] == "string"){
-                    before[pii] = event.data.udo[pii].toString();
-                    event.data.udo[`${prefix}${pii}`] = CryptoES.SHA256(event.data.udo[pii]).toString()
-                    after[pii] = CryptoES.SHA256(event.data.udo[pii]).toString();
+                if (typeof pii[1] == "string"){
+                    before[pii[0]] = pii[1].toString();
+                    event.data.udo[`${prefix}${pii[0]}`] = CryptoES.SHA256(pii[1]).toString();
+                    after[pii[0]] = event.data.udo[`${prefix}${pii[0]}`];
                     
                 }                
-            })
+            });
             console.log(`Before Hashing: \n${JSON.stringify(before, null, 2)}\n`);
             console.log(`After Hashing: \n${JSON.stringify(after, null, 2)}\n`);
             console.log(`All Data: ${JSON.stringify(event.data, null, 2)}`);
         }
     } catch (e){
-        console.log(e)
+        console.log(e);
     }
     console.log(JSON.stringify(event, null, 2));
 })
